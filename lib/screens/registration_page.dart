6eadab7c4824c198629c6b2b252404f4a3168178
Nikita <Nkanunov@gmail.com>
2021-01-28 16:24:09 +0200

    Added BlocBuilder widget

--- lib/screens/registration_page.dart
+++ lib/screens/registration_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:purpleplane_test_app/blocs/auth_bloc/auth.dart';
 import 'package:purpleplane_test_app/models/user.dart';
-import 'package:purpleplane_test_app/services/shared_prefs.dart';
 
 class RegistrationScreen extends StatefulWidget {
   @override
@@ -8,13 +9,13 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
-  SharedPrefs _prefs = SharedPrefs();
+  AuthBloc _authBloc;
   User _user = User();
   bool _obscureText = true;
 
   void saveToSharedPref() async {
-    _user.singUpDate = DateTime.now().toString();
-    await _prefs.save('user', _user.toJson());
+    _authBloc = BlocProvider.of<AuthBloc>(context);
+    _authBloc.add(AuthButtonPressed(user: _user));
     Navigator.pushNamed(context, '/login_screen');
   }
 
@@ -28,143 +29,149 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-              child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xFF73AEF5),
-                Color(0xFF61A4F1),
-                Color(0xFF478DE0),
-                Color(0xFF398AE5),
-              ],
+      child: BlocBuilder<AuthBloc, AuthState>(builder: (context, state) {
+        if (state is InitAuthState || state is Authenticated) {
+          return Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFF73AEF5),
+                  Color(0xFF61A4F1),
+                  Color(0xFF478DE0),
+                  Color(0xFF398AE5),
+                ],
+              ),
             ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Sign up',
-                  style: TextStyle(fontSize: 30, color: Colors.white),
-                ),
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  SizedBox(
-                    height: 30,
-                  ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
                   Text(
-                    'Username',
-                    style: TextStyle(color: Colors.white),
+                    'Sign up',
+                    style: TextStyle(fontSize: 30, color: Colors.white),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 5.0),
-                    child: TextFormField(
-                      autofocus: false,
-                      onChanged: (value) {
-                        setState(() {
-                          _user.login = value;
-                        });
-                      },
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Enter your login',
-                        prefixIcon: Icon(
-                          Icons.login,
-                          color: Colors.white,
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 30,
                         ),
-                        hintStyle: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    'Password',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 5.0),
-                    child: Container(
-                      child: Stack(
-                        alignment: Alignment.centerRight,
-                        children: [
-                          TextFormField(
-                            obscureText: _obscureText,
+                        Text(
+                          'Username',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5.0),
+                          child: TextFormField(
+                            autofocus: false,
                             onChanged: (value) {
                               setState(() {
-                                _user.password = value;
+                                _user.login = value;
                               });
                             },
                             style: TextStyle(color: Colors.white),
                             decoration: InputDecoration(
                               border: OutlineInputBorder(),
-                              hintText: 'Enter your password',
+                              hintText: 'Enter your login',
+                              prefixIcon: Icon(
+                                Icons.login,
+                                color: Colors.white,
+                              ),
                               hintStyle: TextStyle(color: Colors.white),
-                              contentPadding:
-                                  const EdgeInsets.fromLTRB(6, 6, 48, 6),
                             ),
                           ),
-                          IconButton(
-                            icon: Icon(
-                              Icons.lock,
-                              color: Colors.white,
-                            ),
-                            onPressed: () {
-                              seePassword();
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    'Name',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 5.0),
-                    child: TextFormField(
-                      onChanged: (value) {
-                        setState(() {
-                          _user.name = value;
-                        });
-                      },
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Enter your name',
-                        hintStyle: TextStyle(color: Colors.white),
-                        prefixIcon: Icon(
-                          Icons.account_circle,
-                          color: Colors.white,
                         ),
-                      ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          'Password',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5.0),
+                          child: Container(
+                            child: Stack(
+                              alignment: Alignment.centerRight,
+                              children: [
+                                TextFormField(
+                                  obscureText: _obscureText,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _user.password = value;
+                                    });
+                                  },
+                                  style: TextStyle(color: Colors.white),
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Enter your password',
+                                    hintStyle: TextStyle(color: Colors.white),
+                                    contentPadding: const EdgeInsets.fromLTRB(
+                                        6, 6, 48, 6),
+                                  ),
+                                ),
+                                IconButton(
+                                  icon: Icon(
+                                    Icons.lock,
+                                    color: Colors.white,
+                                  ),
+                                  onPressed: () {
+                                    seePassword();
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          'Name',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5.0),
+                          child: TextFormField(
+                            onChanged: (value) {
+                              setState(() {
+                                _user.name = value;
+                              });
+                            },
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Enter your name',
+                              hintStyle: TextStyle(color: Colors.white),
+                              prefixIcon: Icon(
+                                Icons.account_circle,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ]),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    width: double.infinity,
+                    child: RaisedButton(
+                      onPressed: () => saveToSharedPref(),
+                      child: Text('Sign up'),
+                      color: Colors.yellow[900],
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
                     ),
                   ),
-                ]),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 8.0),
-                  width: double.infinity,
-                  child: RaisedButton(
-                    onPressed: () => saveToSharedPref(),
-                    child: Text('Sign up'),
-                    color: Colors.yellow[900],
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0)),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ),
-      ),
-    );
+          );
+        }
+        return null;
+      }),
+    ));
   }
 }
