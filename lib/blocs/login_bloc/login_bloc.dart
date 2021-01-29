2bee4301ed6e2492697860b79bc8c36bd07a551e
Nikita <Nkanunov@gmail.com>
2021-01-29 04:31:37 +0200

    Created login logic in LoginBloc

--- lib/blocs/login_bloc/login_bloc.dart
+++ lib/blocs/login_bloc/login_bloc.dart
@@ -1,17 +1,31 @@
 import 'dart:async';
-import 'dart:js';
 import 'package:bloc/bloc.dart';
-import 'package:flutter/material.dart';
+import 'package:purpleplane_test_app/models/user.dart';
+import 'package:purpleplane_test_app/services/shared_prefs.dart';
 
 import 'login.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   LoginBloc() : super(LoginInitial());
 
+  SharedPrefs _sharedPref = SharedPrefs();
+
+  User user = User();
+
   @override
   Stream<LoginState> mapEventToState(LoginEvent event) async* {
     if (event is LoginButtonPressed) {
-      
+      yield LoginInitial();
+      user = User.fromJson(await _sharedPref.read('user'));
+
+      if (user.login == event.username &&
+          user.password == event.password &&
+          event.username != null &&
+          event.password != null) {
+        yield LoginComplete(user: user);
+      } else {
+        yield LoginFailure(error: 'Error');
+      }
     }
   }
 }
