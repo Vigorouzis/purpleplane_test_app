b72fe3fd9e1d8649e2f243c157915fdd0efd791c
Nikita <Nkanunov@gmail.com>
2021-01-29 04:30:05 +0200

    Added BlocListener and Blocbuilder widgets on LoginScreen

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,7 +1,10 @@
 import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
-import 'package:purpleplane_test_app/models/user.dart';
-import 'package:purpleplane_test_app/services/shared_prefs.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:purpleplane_test_app/blocs/login_bloc/login.dart';
+import 'package:purpleplane_test_app/blocs/login_bloc/login_bloc.dart';
+import 'package:purpleplane_test_app/blocs/login_bloc/login_event.dart';
+import 'package:purpleplane_test_app/screens/main_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -9,148 +12,161 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  User _user = User();
+  LoginBloc _loginBloc;
 
-  var _loginController = TextEditingController();
-  var _passwordController = TextEditingController();
-
-  SharedPrefs _sharedPref = SharedPrefs();
+  String _login;
+  String _password;
   bool _obscureText = true;
 
   void loadFromSharedPrefsAndLogin() async {
-    try {
-      _user = User.fromJson(await _sharedPref.read('user'));
-      if (_user.login == _loginController.text &&
-          _user.password == _passwordController.text) {
-        Navigator.pushNamed(context, '/main_screen');
-      } else {
-        Flushbar(
-          message: 'Login failed',
-          duration: Duration(seconds: 3),
-        )..show(context);
-      }
-    } catch (Excepetion) {
-      throw Exception();
-    }
+    _loginBloc = BlocProvider.of<LoginBloc>(context);
+    _loginBloc.add(LoginButtonPressed(username: _login, password: _password));
   }
 
-   void seePassword() {
+  void seePassword() {
     setState(() {
       _obscureText = !_obscureText;
     });
   }
 
   @override
-  void dispose() {
-    super.dispose();
-    _loginController.dispose();
-    _passwordController.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-              child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xFF73AEF5),
-                Color(0xFF61A4F1),
-                Color(0xFF478DE0),
-                Color(0xFF398AE5),
-              ],
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Login',
-                  style: TextStyle(fontSize: 30, color: Colors.white),
+        child: BlocListener<LoginBloc, LoginState>(
+          listener: (context, state) {
+            if (state is LoginFailure) {
+              return Flushbar(
+                message: 'Login failed',
+                duration: Duration(seconds: 3),
+              )..show(context);
+            }
+
+            if (state is LoginComplete) {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MainScreen(),
+                  ));
+            }
+          },
+          child: BlocBuilder<LoginBloc, LoginState>(builder: (context, state) {
+            print(state);
+            if (state is LoginInitial ||
+                state is LoginComplete ||
+                state is LoginFailure) {
+              return Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xFF73AEF5),
+                      Color(0xFF61A4F1),
+                      Color(0xFF478DE0),
+                      Color(0xFF398AE5),
+                    ],
+                  ),
                 ),
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    'Username',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 5.0),
-                    child: TextFormField(
-                      controller: _loginController,
-                      autofocus: false,
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Enter your login',
-                        prefixIcon: Icon(
-                          Icons.login,
-                          color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Login',
+                        style: TextStyle(fontSize: 30, color: Colors.white),
+                      ),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 30,
+                            ),
+                            Text(
+                              'Username',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 5.0),
+                              child: TextFormField(
+                                onChanged: (value) {
+                                  _login = value;
+                                },
+                                autofocus: false,
+                                style: TextStyle(color: Colors.white),
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Enter your login',
+                                  prefixIcon: Icon(
+                                    Icons.login,
+                                    color: Colors.white,
+                                  ),
+                                  hintStyle: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            Text(
+                              'Password',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 5.0),
+                              child: Container(
+                                child: Stack(
+                                  alignment: Alignment.centerRight,
+                                  children: [
+                                    TextFormField(
+                                      onChanged: (value) {
+                                        _password = value;
+                                      },
+                                      obscureText: _obscureText,
+                                      style: TextStyle(color: Colors.white),
+                                      decoration: InputDecoration(
+                                        border: OutlineInputBorder(),
+                                        hintText: 'Enter your password',
+                                        hintStyle:
+                                            TextStyle(color: Colors.white),
+                                        contentPadding:
+                                            const EdgeInsets.fromLTRB(
+                                                6, 6, 48, 6),
+                                      ),
+                                    ),
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.lock,
+                                        color: Colors.white,
+                                      ),
+                                      onPressed: () {
+                                        seePassword();
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ]),
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 8.0),
+                        width: double.infinity,
+                        child: RaisedButton(
+                          onPressed: () => loadFromSharedPrefsAndLogin(),
+                          child: Text('Login'),
+                          color: Colors.green,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0)),
                         ),
-                        hintStyle: TextStyle(color: Colors.white),
                       ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    'Password',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                   Padding(
-                    padding: const EdgeInsets.only(top: 5.0),
-                    child: Container(
-                      child: Stack(
-                        alignment: Alignment.centerRight,
-                        children: [
-                          TextFormField(
-                            controller: _passwordController,
-                            obscureText: _obscureText,
-                            style: TextStyle(color: Colors.white),
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              hintText: 'Enter your password',
-                              hintStyle: TextStyle(color: Colors.white),
-                              contentPadding:
-                                  const EdgeInsets.fromLTRB(6, 6, 48, 6),
-                            ),
-                          ),
-                          IconButton(
-                            icon: Icon(
-                              Icons.lock,
-                              color: Colors.white,
-                            ),
-                            onPressed: () {
-                              seePassword();
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ]),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 8.0),
-                  width: double.infinity,
-                  child: RaisedButton(
-                    onPressed: () => loadFromSharedPrefsAndLogin(),
-                    child: Text('Login'),
-                    color: Colors.green,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0)),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
+              );
+            }
+            return null;
+          }),
         ),
       ),
     );
